3579a45c221f3f6cc68f0b57ed1e8e8295514179
Darin Webster <darinleenavy@gmail.com>
2019-06-24 20:23:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -54,7 +54,7 @@ end
 def reduce_to_all_true(source_array)
   source_array = [1, 2, true, "razmatazz"]
   
-  reduce_to_all_true = source_array.reduce { |true, razmatazz| sum + razmatazz }
+  reduce_to_all_true = source_array.reduce { |true| total = true }
 end
 
 
