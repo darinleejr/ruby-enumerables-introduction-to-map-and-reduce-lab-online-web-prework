fe91f5bc7f7327281ed7614615162b7ec3820f8b
Darin Webster <darinleenavy@gmail.com>
2019-06-24 20:33:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -64,8 +64,10 @@ def reduce_to_all_true(source_array)
 end
 
 def reduce_to_any_true(source_array)
-  source_array = 
+  source_array = [false, nil, nil, nil, true]
 
+  reduce_to_any_true = source_array.reduce { |truthy| total = true
+end
 
 
 
